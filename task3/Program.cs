19d8a17256522b8796c87829e2db0e2e8dca6232
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-02-20 19:20:16 +0300

    решили задачу 3

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("задайте номер четверти");
+int number = int.Parse(Console.ReadLine());
+if (number == 1) Console.WriteLine("x>0,y>0");
+else if (number == 2) Console.WriteLine("x<0,y>0");
+else if (number == 3) Console.WriteLine("x<0,y<0");
+else if (number == 4) Console.WriteLine("x>0,y<0");
+else Console.WriteLine("вышли за дипазон");
+
