586a7c552c32af6c3765ae259e5b87b2e4756beb
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-02-20 19:01:47 +0300

    решили задачу2

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите число a ");
+int numbera = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число b");
+int numberb = int.Parse(Console.ReadLine());
+if (numberb == numbera * numbera) Console.WriteLine("true");
+else Console.WriteLine("false");
+
